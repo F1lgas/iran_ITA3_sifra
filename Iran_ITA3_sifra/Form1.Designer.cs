25ab791207888e04ce2e1333a1daf6f5fb0ff989
Filip Iran <iranf.03@spst.cz>
2022-01-18 18:10:08 +0100

    v1.0

--- Iran_ITA3_sifra/Form1.Designer.cs
+++ Iran_ITA3_sifra/Form1.Designer.cs
@@ -28,13 +28,104 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.desifrovat = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.rot = new System.Windows.Forms.TextBox();
+            this.vstup = new System.Windows.Forms.TextBox();
+            this.sifra = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // desifrovat
+            // 
+            this.desifrovat.Location = new System.Drawing.Point(140, 39);
+            this.desifrovat.Name = "desifrovat";
+            this.desifrovat.Size = new System.Drawing.Size(75, 22);
+            this.desifrovat.TabIndex = 13;
+            this.desifrovat.Text = "Dešifrovat";
+            this.desifrovat.UseVisualStyleBackColor = true;
+            this.desifrovat.Click += new System.EventHandler(this.desifrovat_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(92, 44);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(42, 13);
+            this.label3.TabIndex = 12;
+            this.label3.Text = "písmen";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 44);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(51, 13);
+            this.label2.TabIndex = 11;
+            this.label2.Text = "Rotace o";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 15);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(31, 13);
+            this.label1.TabIndex = 10;
+            this.label1.Text = "Šifra:";
+            // 
+            // rot
+            // 
+            this.rot.Location = new System.Drawing.Point(69, 41);
+            this.rot.Name = "rot";
+            this.rot.Size = new System.Drawing.Size(17, 20);
+            this.rot.TabIndex = 9;
+            // 
+            // vstup
+            // 
+            this.vstup.Location = new System.Drawing.Point(49, 12);
+            this.vstup.Name = "vstup";
+            this.vstup.Size = new System.Drawing.Size(85, 20);
+            this.vstup.TabIndex = 8;
+            // 
+            // sifra
+            // 
+            this.sifra.Location = new System.Drawing.Point(140, 11);
+            this.sifra.Name = "sifra";
+            this.sifra.Size = new System.Drawing.Size(75, 22);
+            this.sifra.TabIndex = 7;
+            this.sifra.Text = "Šifrovat";
+            this.sifra.UseVisualStyleBackColor = true;
+            this.sifra.Click += new System.EventHandler(this.sifra_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(226, 73);
+            this.Controls.Add(this.desifrovat);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.rot);
+            this.Controls.Add(this.vstup);
+            this.Controls.Add(this.sifra);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button desifrovat;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox rot;
+        private System.Windows.Forms.TextBox vstup;
+        private System.Windows.Forms.Button sifra;
     }
 }
 
--- Iran_ITA3_sifra/Form1.cs
+++ Iran_ITA3_sifra/Form1.cs
@@ -16,5 +16,137 @@ namespace Iran_ITA3_sifra
         {
             InitializeComponent();
         }
+
+        private void sifra_Click(object sender, EventArgs e)
+        {
+            char[] sifra = vstup.Text.ToCharArray();
+            int celkem = sifra.GetLength(0);
+            string vys = "";
+
+            string rot3 = Convert.ToString(rot.Text);
+
+            try
+            {
+                int rot2 = Convert.ToInt32(rot.Text);
+
+                for (int i = 0; i < celkem; i++)
+                {
+                    int x = 0;
+                    if (sifra[i] >= 'a' && sifra[i] <= 'm')
+                    {
+                        x = 1;
+                    }
+                    else if (sifra[i] >= 'n' && sifra[i] <= 'z')
+                    {
+                        x = 2;
+                    }
+                    else if (sifra[i] >= 'A' && sifra[i] <= 'M')
+                    {
+                        x = 1;
+                    }
+                    else if (sifra[i] >= 'N' && sifra[i] <= 'Z')
+                    {
+                        x = 2;
+                    }
+
+                    switch (x)
+                    {
+                        case 1:
+                            {
+                                sifra[i] = (char)((int)sifra[i] + rot2);
+                                break;
+                            }
+                        case 2:
+                            {
+                                if (sifra[i] > 122)
+                                {
+                                    sifra[i] = (char)((int)sifra[i] - 26);
+                                }
+
+                                sifra[i] = (char)((int)sifra[i] - rot2);
+                                break;
+                            }
+                        default:
+                            {
+                                break;
+                            }
+                    }
+
+                    vys += sifra[i];
+                }
+
+                MessageBox.Show(vys, "Výsledek", MessageBoxButtons.OK);
+            }
+            catch
+            {
+                MessageBox.Show("Zadej hodnoty!", "Error");
+            }
+        }
+
+        private void desifrovat_Click(object sender, EventArgs e)
+        {
+            char[] sifra = vstup.Text.ToCharArray();
+            int celkem = sifra.GetLength(0);
+            string vys = "";
+
+            string rot3 = Convert.ToString(rot.Text);
+
+            try
+            {
+                int rot2 = 26 - Convert.ToInt32(rot.Text);
+
+                for (int i = 0; i < celkem; i++)
+                {
+                    int x = 0;
+                    if (sifra[i] >= 'a' && sifra[i] <= 'm')
+                    {
+                        x = 1;
+                    }
+                    else if (sifra[i] >= 'n' && sifra[i] <= 'z')
+                    {
+                        x = 2;
+                    }
+                    else if (sifra[i] >= 'A' && sifra[i] <= 'M')
+                    {
+                        x = 1;
+                    }
+                    else if (sifra[i] >= 'N' && sifra[i] <= 'Z')
+                    {
+                        x = 2;
+                    }
+
+                    switch (x)
+                    {
+                        case 1:
+                            {
+                                sifra[i] = (char)((int)sifra[i] + rot2);
+                                break;
+                            }
+                        case 2:
+                            {
+                                if (sifra[i] > 122)
+                                {
+                                    sifra[i] = (char)((int)sifra[i] - 26);
+                                }
+
+                                sifra[i] = (char)((int)sifra[i] - rot2);
+                                break;
+                            }
+                        default:
+                            {
+                                break;
+                            }
+                    }
+
+                    vys += sifra[i];
+                }
+
+                MessageBox.Show(vys, "Výsledek", MessageBoxButtons.OK);
+            }
+            catch
+            {
+                MessageBox.Show("Zadej hodnoty!", "Error");
+            }
+        }
     }
 }
